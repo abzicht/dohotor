a382c80cbfe7096cc8679f3e7d11f72dc967a7aa
Abzicht <abzicht@gmail.com>
2021-07-25 15:57:41 +0200

    added sleep time to ensure tor is up before dnscrypt starts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting Tor"
 tor &
 
 echo "Starting DNSCrypt"
-dnscrypt-proxy -config /app/dnscrypt-proxy.toml -service start
+sleep 50 && dnscrypt-proxy -config /app/dnscrypt-proxy.toml -service start
 
 echo "Running dnsmasq in foreground"
 dnsmasq --keep-in-foreground --conf-file=/app/dnsmasq.conf \
